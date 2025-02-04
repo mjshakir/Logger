eb22ed4b69934c79c8b466b9b9be4610b73aaaf5
mjshakir <maslove23@gmail.com>
2025-02-03 17:25:28 -0800

    Change: use to sqlite3_stmt for faster log

--- include/SQLogger.hpp
+++ include/SQLogger.hpp
@@ -4,11 +4,14 @@
 //--------------------------------------------------------------
 #include <optional>
 #include <memory>
-#include <mutex>
+#include <string_view>
+// #include <mutex>
 //--------------------------------------------------------------
 // Forward declarations for SQLite
 //--------------------------------------------------------------
 struct sqlite3;
+//--------------------------
+struct sqlite3_stmt;
 //--------------------------
 extern "C" {
     int sqlite3_close(sqlite3*);
@@ -29,8 +32,18 @@ namespace Logger {
             bool initialize(void);
             //--------------------------
             bool log_message(std::string_view level, std::string_view message, const std::optional<std::chrono::system_clock::time_point>& now) const;
-           //--------------------------
-            constexpr std::string_view create_table_sql(void) const; 
+            //--------------------------
+            constexpr std::string_view create_table_sql(void) const;
+            //--------------------------
+            constexpr std::string_view default_time(void) const;
+            //--------------------------
+            std::string format_time(const std::optional<std::chrono::system_clock::time_point>& now) const;
+            //--------------------------
+            bool sql_reset(std::string_view time, std::string_view level, std::string_view message) const;
+            //--------------------------
+            struct SQLiteStmtDeleter {
+                void operator()(sqlite3_stmt* stmt) const;
+            };// end struct SQLiteStmtDeleter
             //--------------------------------------------------------------
         private:
             //--------------------------------------------------------------
@@ -43,6 +56,8 @@ namespace Logger {
             SQLogger& operator=(SQLogger&&)         = delete;
             //--------------------------
             std::unique_ptr<sqlite3, decltype(&sqlite3_close)> m_db;
+            std::unique_ptr<sqlite3_stmt, SQLiteStmtDeleter> m_insertStmt;
+            //--------------------------
             const bool m_initialized;
             //--------------------------
             // std::mutex m_mutex;
