170cf2ed4fceddfdce83b3f16be44eb4c8ef5c41
mjshakir <26232377+mjshakir@users.noreply.github.com>
2025-12-27 18:59:34 -0800

    [REMOVE]: macro code.

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -171,7 +171,7 @@ namespace Logger {
                 //--------------------------
                 { // protect the log_file call
                     std::lock_guard<std::mutex> lock(m_mutex);
-                    level_message(level, std::move(_formatted_message), now);
+                    level_message(level, message, _formatted_message, std::nullopt, now);
                 } // end protect the log_file call
                 //--------------------------
             }// end void log(LogLevel level, std::string_view format, Args&&... args)
@@ -191,12 +191,12 @@ namespace Logger {
                 //--------------------------
                 { // protect the log_file call
                     std::lock_guard<std::mutex> lock(m_mutex);
-                    level_message(level, std::move(_formatted_message), now);
+                    level_message(level, message, _formatted_message, function_name, now);
                 } // end protect the log_file call
                 //--------------------------
             }// end void log(LogLevel level, std::string_view function_name, std::string_view format, Args&&... args)
             //--------------------------
-            void level_message(const LogLevel& level, std::string_view message, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
+            void level_message(const LogLevel& level, std::string_view raw_message, std::string_view formatted_message, const std::optional<std::string_view>& function_name = std::nullopt, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
             //--------------------------
             constexpr std::string_view level_name(const LogLevel& level) const;
             //--------------------------
@@ -207,6 +207,8 @@ namespace Logger {
             std::string format_message(const LogLevel& level, std::string_view message, const std::chrono::system_clock::time_point& now) const;
             //--------------------------
             std::string format_message(const LogLevel& level, std::string_view function_name, std::string_view message, const std::chrono::system_clock::time_point& now) const;
+            //--------------------------
+            std::string format_timestamp(const std::tm& timeinfo) const;
             //--------------------------
             void log_file(std::string_view filename, std::string_view message, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
             //--------------------------
@@ -228,7 +230,7 @@ namespace Logger {
                 //--------------------------
                 { // protect the log_file call
                     std::lock_guard<std::mutex> lock(m_mutex);
-                    level_message(level, std::move(_formatted_message), now);
+                    level_message(level, _message, _formatted_message, std::nullopt, now);
                 } // end protect the log_file call
                 //--------------------------
             }// end void log_stream(LogLevel level, std::string_view message, const T& container)
@@ -249,7 +251,7 @@ namespace Logger {
                 //--------------------------
                 { // protect the log_file call
                     std::lock_guard<std::mutex> lock(m_mutex);
-                    level_message(level, std::move(_formatted_message), now);
+                    level_message(level, _message, _formatted_message, function_name, now);
                 } // end protect the log_file call
                 //--------------------------
             }// end void log_stream(LogLevel level, std::string_view message, const T& container)
@@ -358,7 +360,7 @@ namespace Logger {
             //--------------------------
             void get_time(std::tm* timeinfo, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
             //--------------------------
-            void logs(const LogLevel& level, std::string_view message, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
+            void logs(const LogLevel& level, [[maybe_unused]] std::string_view raw_message, std::string_view formatted_message, [[maybe_unused]] const std::optional<std::string_view>& function_name = std::nullopt, const std::optional<std::chrono::system_clock::time_point>& now = std::nullopt) const;
             //--------------------------------------------------------------
         private:
             //--------------------------------------------------------------
@@ -376,164 +378,4 @@ namespace Logger {
     //--------------------------------------------------------------
 } // end namespace Logger
 //--------------------------------------------------------------
-// Macros for Automatically Capturing Function Name and Class**
-//--------------------------------------------------------------
-#ifdef __GNUC__  // GCC/Clang
-    #define FUNC_NAME __PRETTY_FUNCTION__ // __func__ __FUNCTION__ __PRETTY_FUNCTION__
-#elif defined(_MSC_VER)  // MSVC
-    #define FUNC_NAME __FUNCTION__
-#else
-    #define FUNC_NAME "UnknownFunction"
-#endif
-//--------------------------------------------------------------
-// Existing logging macros
-//--------------------------------------------------------------
-#define LOG_ERROR(msg, ...) Logger::Logger::instance().error(msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_WARNING(msg, ...) Logger::Logger::instance().warning(msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_INFO(msg, ...) Logger::Logger::instance().info(msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_ERROR_STREAM(msg, container) Logger::Logger::instance().error_stream(msg, container)
-#define LOG_WARNING_STREAM(msg, container) Logger::Logger::instance().warning_stream(msg, container)
-#define LOG_INFO_STREAM(msg, container) Logger::Logger::instance().info_stream(msg, container)
-//--------------------------------------------------------------
-// **Helper macros for unique variable names using __LINE__**
-//--------------------------------------------------------------
-#define CONCATENATE_DETAIL(x, y) x##y
-#define CONCATENATE(x, y) CONCATENATE_DETAIL(x, y)
-#define UNIQUE_VAR(base) CONCATENATE(base, __LINE__)
-//--------------------------------------------------------------
-// **Logging macros for conditional logging with DEBUG**
-//--------------------------------------------------------------
-#ifdef LOGGER_DEBUG
-    #define LOG_DEBUG(msg, ...) Logger::Logger::instance().debug(msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_DEBUG_STREAM(msg, container) Logger::Logger::instance().debug_stream(msg, container)
-    //--------------------------
-    // **Newly added macros**
-    #define LOG_WARNING_DEBUG(msg, ...) Logger::Logger::instance().warning(msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_WARNING_DEBUG_STREAM(msg, container) Logger::Logger::instance().warning_stream(msg, container)
-    //--------------------------
-    #define LOG_ERROR_DEBUG(msg, ...) Logger::Logger::instance().error(msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_ERROR_DEBUG_STREAM(msg, container) Logger::Logger::instance().error_stream(msg, container)
-#else
-    #define LOG_DEBUG(msg, ...)
-    #define LOG_DEBUG_STREAM(msg, container)
-    //--------------------------
-    // **Ensure macros do nothing when DEBUG is not defined**
-    #define LOG_WARNING_DEBUG(msg, ...)
-    #define LOG_WARNING_DEBUG_STREAM(msg, container)
-    //--------------------------
-    #define LOG_ERROR_DEBUG(msg, ...)
-    #define LOG_ERROR_DEBUG_STREAM(msg, container)
-#endif
-
-//--------------------------------------------------------------
-// **Logging macros for logging messages only once**
-//--------------------------------------------------------------
-#define LOG_ONCE_IMPL(level_method, flag_var, ...) do { \
-    static std::once_flag flag_var; \
-    std::call_once(flag_var, [&]{ \
-        Logger::Logger::instance().level_method(__VA_ARGS__); \
-    }); \
-} while(0)
-
-#define LOG_ONCE(level_method, ...) LOG_ONCE_IMPL(level_method, UNIQUE_VAR(log_once_flag_), __VA_ARGS__)
-
-#define LOG_ONCE_STREAM_IMPL(level_method, flag_var, msg, container) do { \
-    static std::once_flag flag_var; \
-    std::call_once(flag_var, [&]{ \
-        Logger::Logger::instance().level_method(msg, container); \
-    }); \
-} while(0)
-
-#define LOG_ONCE_STREAM(level_method, msg, container) \
-    LOG_ONCE_STREAM_IMPL(level_method, UNIQUE_VAR(log_once_flag_), msg, container)
-
-// **Define macros for each log level**
-#define LOG_ERROR_ONCE(...) LOG_ONCE(error, __VA_ARGS__)
-#define LOG_WARNING_ONCE(...) LOG_ONCE(warning, __VA_ARGS__)
-#define LOG_INFO_ONCE(...) LOG_ONCE(info, __VA_ARGS__)
-
-#define LOG_ERROR_ONCE_STREAM(msg, container) LOG_ONCE_STREAM(error_stream, msg, container)
-#define LOG_WARNING_ONCE_STREAM(msg, container) LOG_ONCE_STREAM(warning_stream, msg, container)
-#define LOG_INFO_ONCE_STREAM(msg, container) LOG_ONCE_STREAM(info_stream, msg, container)
-
-#ifdef LOGGER_DEBUG
-    #define LOG_DEBUG_ONCE(...) LOG_ONCE(debug, __VA_ARGS__)
-    #define LOG_DEBUG_ONCE_STREAM(msg, container) LOG_ONCE_STREAM(debug_stream, msg, container)
-#else
-    #define LOG_DEBUG_ONCE(...)
-    #define LOG_DEBUG_ONCE_STREAM(msg, container)
-#endif
-//--------------------------------------------------------------i
-// Logging With Function Name and Class
-//--------------------------------------------------------------
-#define LOG_ERROR_FUNCTION(msg, ...) Logger::Logger::instance().error_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_WARNING_FUNCTION(msg, ...) Logger::Logger::instance().warning_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_INFO_FUNCTION(msg, ...) Logger::Logger::instance().info_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-#define LOG_ERROR_FUNCTION_STREAM(msg, container) Logger::Logger::instance().error_stream_function(FUNC_NAME, msg, container)
-#define LOG_WARNING_FUNCTION_STREAM(msg, container) Logger::Logger::instance().warning_stream_function(FUNC_NAME, msg, container)
-#define LOG_INFO_FUNCTION_STREAM(msg, container) Logger::Logger::instance().info_stream_function(FUNC_NAME, msg, container)
-//--------------------------------------------------------------
-// **Logging macros for conditional logging with DEBUG**
-//--------------------------------------------------------------
-#ifdef LOGGER_DEBUG
-    #define LOG_DEBUG_FUNCTION(msg, ...) Logger::Logger::instance().debug_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_DEBUG_FUNCTION_STREAM(msg, container) Logger::Logger::instance().debug_stream_function(FUNC_NAME, msg, container)
-    //--------------------------
-    // **Newly added macros**
-    #define LOG_WARNING_DEBUG_FUNCTION(msg, ...) Logger::Logger::instance().warning_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_WARNING_DEBUG_FUNCTION_STREAM(msg, container) Logger::Logger::instance().warning_stream_function(FUNC_NAME, msg, container)
-    //--------------------------
-    #define LOG_ERROR_DEBUG_FUNCTION(msg, ...) Logger::Logger::instance().error_function(FUNC_NAME, msg __VA_OPT__(,) __VA_ARGS__)
-    #define LOG_ERROR_DEBUG_FUNCTION_STREAM(msg, container) Logger::Logger::instance().error_stream_function(FUNC_NAME, msg, container)
-#else
-    #define LOG_DEBUG_FUNCTION(msg, ...)
-    #define LOG_DEBUG_FUNCTION_STREAM(msg, container)
-    //--------------------------
-    // **Ensure macros do nothing when DEBUG is not defined**
-    #define LOG_WARNING_DEBUG_FUNCTION(msg, ...)
-    #define LOG_WARNING_DEBUG_FUNCTION_STREAM(msg, container)
-    //--------------------------
-    #define LOG_ERROR_DEBUG_FUNCTION(msg, ...)
-    #define LOG_ERROR_DEBUG_FUNCTION_STREAM(msg, container)
-#endif
-
-//--------------------------------------------------------------
-// **Logging macros for logging messages only once**
-//--------------------------------------------------------------
-#define LOG_ONCE_FUNCTION_IMPL(level_method, flag_var, ...) do { \
-    static std::once_flag flag_var; \
-    std::call_once(flag_var, [&]{ \
-        Logger::Logger::instance().level_method(FUNC_NAME, __VA_ARGS__); \
-    }); \
-} while(0)
-
-#define LOG_ONCE_FUNCTION(level_method, ...) \
-    LOG_ONCE_FUNCTION_IMPL(level_method, UNIQUE_VAR(log_once_flag_), __VA_ARGS__)
-
-#define LOG_ONCE_FUNCTION_STREAM_IMPL(level_method, flag_var, msg, container) do { \
-    static std::once_flag flag_var; \
-    std::call_once(flag_var, [&]{ \
-        Logger::Logger::instance().level_method(FUNC_NAME, msg, container); \
-    }); \
-} while(0)
-
-#define LOG_ONCE_FUNCTION_STREAM(level_method, msg, container) \
-    LOG_ONCE_FUNCTION_STREAM_IMPL(level_method, UNIQUE_VAR(log_once_flag_), msg, container)
-
-// **Define macros for each log level**
-#define LOG_ERROR_FUNCTION_ONCE(...) LOG_ONCE_FUNCTION(error_function, __VA_ARGS__)
-#define LOG_WARNING_FUNCTION_ONCE(...) LOG_ONCE_FUNCTION(warning_function, __VA_ARGS__)
-#define LOG_INFO_FUNCTION_ONCE(...) LOG_ONCE_FUNCTION(info_function, __VA_ARGS__)
-
-#define LOG_ERROR_ONCE_FUNCTION_STREAM(msg, container) LOG_ONCE_FUNCTION_STREAM(error_stream_function, msg, container)
-#define LOG_WARNING_ONCE_FUNCTION_STREAM(msg, container) LOG_ONCE_FUNCTION_STREAM(warning_stream_function, msg, container)
-#define LOG_INFO_ONCE_FUNCTION_STREAM(msg, container) LOG_ONCE_FUNCTION_STREAM(info_stream_function, msg, container)
-
-#ifdef LOGGER_DEBUG
-    #define LOG_DEBUG_FUNCTION_ONCE(...) LOG_ONCE_FUNCTION(debug_function, __VA_ARGS__)
-    #define LOG_DEBUG_ONCE_FUNCTION_STREAM(msg, container) LOG_ONCE_FUNCTION_STREAM(debug_stream_function, msg, container)
-#else
-    #define LOG_DEBUG_FUNCTION_ONCE(...)
-    #define LOG_DEBUG_ONCE_FUNCTION_STREAM(msg, container)
-#endif
-//--------------------------------------------------------------
+// Logging macros are defined in `log.hpp`.
